aa175797ed4071eb3a934348be582781e963d496
unknown <mejhasuraj@gmail.com>
2020-12-22 17:37:03 +0530

    heroku website hosting

---
+++ src/db/conn.js
@@ -0,0 +1,13 @@
+const mongoose =require("mongoose");
+
+
+mongoose.connect("mongodb://localhost:27017/thapadynamic",{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+
+}).then(()=>{
+    console.log("connection successful");
+}).catch((error)=>{
+    console.log(error);
+})
---
+++ src/models/usermessage.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+const validator =require("validator");
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLength:3
+
+    },
+    email:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email ha laure")
+            }
+        }
+
+    },
+    phone:{
+        type:Number,
+        required:true,
+        min:10
+
+    },
+    message:{
+        type:String,
+        required:true,
+        minLength:10
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+
+
+});
+// create collection 
+const User=mongoose.model("User",userSchema);
+module.exports=User;
